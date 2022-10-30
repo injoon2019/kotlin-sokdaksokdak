f817bc370f80ee1cf0f159592e2ef3fd3621462a
injoon2019 <ison@sfu.ca>
2022-10-30 21:10:57 +0900

    feat: Caffeine Cache 적용

--- src/main/kotlin/com/example/sokdak/member/controller/MemberController.kt
+++ src/main/kotlin/com/example/sokdak/member/controller/MemberController.kt
@@ -2,13 +2,11 @@ package com.example.sokdak.member.controller
 
 import com.example.sokdak.member.dto.request.CreateMemberRequest
 import com.example.sokdak.member.dto.response.CreateMemberResponse
+import com.example.sokdak.member.dto.response.FindMemberResponse
 import com.example.sokdak.member.service.MemberService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.net.URI
 
 @RestController
@@ -21,5 +19,10 @@ class MemberController(
     fun singUp(@RequestBody createMemberRequest: CreateMemberRequest): ResponseEntity<Unit> {
         val createdMemberResponse = memberService.signUp(createMemberRequest)
         return ResponseEntity.created(URI.create("/members/" + createdMemberResponse.id)).build();
+    }
+
+    @GetMapping("/{id}")
+    fun findMemberById(@PathVariable id: Long): ResponseEntity<FindMemberResponse> {
+        return ResponseEntity.ok(memberService.findMemberById(id))
     }
 }
