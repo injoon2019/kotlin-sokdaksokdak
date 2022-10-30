4448a143502d5cc794d66402270040c338a180c9
injoon2019 <ison@sfu.ca>
2022-10-30 16:26:44 +0900

    chore: kotest 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,10 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.kotest", "kotest-runner-junit5", "5.2.3")
+	//testImplementation("io.kotest", "kotest-runner-junit5", "5.2.3")
+	testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
+	testImplementation("io.kotest:kotest-assertions-core:5.4.2")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 }
 
 dependencyManagement {
